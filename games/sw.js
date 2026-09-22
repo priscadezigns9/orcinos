9f7909d7ae5fba7422178e8c2518829ab7a02ba0
PD <priscadezigns9@gmail.com>
2026-09-21 22:07:29 -0400

    RPS: Point PWA at latest multiplayer build

--- games/sw.js
+++ games/sw.js
@@ -1,5 +1,5 @@
-const CACHE='rps-duel-v6';
-const SHELL=['./rps-game-v2.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='rps-duel-v7';
+const SHELL=['./rps-game-v3.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match('./rps-game-v2.html'))))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match('./rps-game-v3.html'))))});
