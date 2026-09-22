b8c34ad16cd9370d1578df4497c600ab8314b323
PD <priscadezigns9@gmail.com>
2026-09-21 21:51:27 -0400

    RPS: Point PWA to refreshed game route

--- games/sw.js
+++ games/sw.js
@@ -1,5 +1,5 @@
-const CACHE='rps-duel-v3';
-const SHELL=['./rps.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='rps-duel-v4';
+const SHELL=['./rps-game.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match('./rps.html'))))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match('./rps-game.html'))))});
