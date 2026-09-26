b2433e6ca9d70f5ddf7bd27176a99e75aa955342
PD <priscadezigns9@gmail.com>
2026-09-26 16:32:55 -0400

    RPS: Keep play action visible and use mascot app icon

--- games/sw.js
+++ games/sw.js
@@ -1,4 +1,4 @@
-const CACHE='rps-duel-v9';
+const CACHE='rps-duel-v10';
 const SHELL=['./rps-live.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
